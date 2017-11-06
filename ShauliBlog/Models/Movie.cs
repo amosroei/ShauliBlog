ae66747d23a2517f18fc46f26cc2eadda8ec2a8f
roei7 <roei7@DESKTOP-Q19M4HI>
2017-11-06 19:31:21 +0200

    add movie entity

---
+++ ShauliBlog/Models/Movie.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace ShauliBlog.Models
+{
+    public class Movie
+    {
+        [Key]
+        public int MovieID { get; set; }
+        public string MovieName { get; set; }
+        public string MovieDirectorName{ get; set; }
+        public int MovieReleaseYear{ get; set; }
+        public int MovieGenreId { get; set; }        
+    }
+}
