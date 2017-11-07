d1513fa2c53efcac668c9eb4903b44f992aa1a59
roei7 <roei7@DESKTOP-Q19M4HI>
2017-11-07 21:17:21 +0200

    fix comment not saving bug

--- ShauliBlog/Controllers/CommentController.cs
+++ ShauliBlog/Controllers/CommentController.cs
@@ -34,7 +34,7 @@ namespace ShauliBlog.Controllers
         public ActionResult Create(Comment comment)
         {
             // check if the ccomment is valid
-            if ((ModelState.IsValid)&& (comment.Account != null))
+            if (ModelState.IsValid)
             {
                 // sets the comment date to the current date and time
                 comment.CommentDate = DateTime.Now;
